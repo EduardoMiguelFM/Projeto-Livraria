02791ab83c6875240d159f2ee73264d9d1043def
edumiguel <dudumfmonteiro@gmail.com>
2024-03-15 09:54:15 -0300

    Metódo Construtor

--- src/CriarLivros.java
+++ src/CriarLivros.java
@@ -4,30 +4,27 @@ public class CriarLivros {
         // Instanciando um objeto = criar o objeto na memória
         // tipoObjeto nomeObejto = operadorInstaciação construtor
         // Livro      favorito =   new                 Livro();
+        Editora editora = new Editora();
+        editora.nome ="VR Editora; 1ª edição";
+        editora.representane = "João";
 
-        Livro favorito = new Livro();
-        favorito.titulo = "Diário de um Banana";
-        favorito.autor ="Jeff Kinney";
-        favorito.valor = 49.50;
-        favorito.editora ="VR Editora; 1ª edição";
-        favorito.resumo ="Não é fácil ser criança...";
-        favorito.tipoCapa ="Dura";
-        //capa: comum, capaDura, digital
+        Livro favorito = new Livro("Diário de um Banana", "Jeff Kinney", 49.50, editora ,50, "Não é fácil ser criança...", "Dura" );
+
+        //favorito.titulo = "Diário de um Banana";
+        //favorito.autor ="Jeff Kinney";
+        //favorito.valor = 49.50;
+        //favorito.editora ="VR Editora; 1ª edição";
+        //favorito.paginas = 50;
+        //favorito.resumo ="Não é fácil ser criança...";
+        //favorito.tipoCapa ="Dura";
+
 
         System.out.println("Meu livro favorito é: " +
         favorito.titulo + "\nO seu autor é:" + favorito.autor +
         "\nPreço: R$ " + favorito.valor);
 
-
-
-
-
-
-
-
-
-
-        //Livro meuLivro = new Livro();
+        System.out.println(favorito.exibirDados());
 
     }
+
 }
--- src/Livro.java
+++ src/Livro.java
@@ -4,19 +4,43 @@ public class Livro {
     public String titulo;
     public String autor;
     public double valor;
-    public String editora;
+    public Editora editora;
     public int paginas;
     public String resumo;
     public String tipoCapa;
+    public String exibirDados;
 
-
-   // public boolean capaComum;
-   // public  String tipoCapaDefinida(){
-        //if(capaComum==true);
-              // return "Comum";
-        //else
-              // return "Capa Dura";
-    //}
 
     // ---Métodos--
+    public String exibirDados() {
+        String dados = "" +
+                "--------------" +
+                "\nTítulo: " + this.titulo +
+                "\nResumo: " + this.resumo +
+                "\nPreço: R$" + this.valor +
+                "\nPáginas: " + this.paginas +
+                "\nAutor: " + this.autor +
+                "\nEditora: " + this.editora.nome+
+                "\nRepresentante da Editora: " + this.editora.representane+
+                "\n-------------";
+        return dados;
+
+         //public Livro(String titulo, double preco){
+             //this.titulo = titulo;
+             //this.valor = preco;
+        };
+    //public Livro(String titulo, double preco){
+        //this.titulo = titulo;
+        //this.valor = preco;
+
+    public Livro(String titulo, String autor, double valor, Editora editora, int paginas, String resumo, String tipoCapa) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.valor = valor;
+        this.editora = editora;
+        this.paginas = paginas;
+        this.resumo = resumo;
+        this.tipoCapa = tipoCapa;
+    }
+
 }
